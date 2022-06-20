c2424d9a1241de0dda55b6bce384ebb1e8f65888
Arturas0 <arturas.bnr@gmail.com>
2022-06-20 21:19:24 +0300

    wip

--- app/Http/Controllers/TruckController.php
+++ app/Http/Controllers/TruckController.php
@@ -1,32 +1,23 @@
 <?php
 
+declare(strict_types=1);
+
 namespace App\Http\Controllers;
 
 use App\Models\Truck;
 use App\Models\Mechanic;
 use Illuminate\Http\Request;
-use App\Http\Requests\TruckUpdateRequest;
+use App\Http\Requests\TruckUpsertRequest;
+use Illuminate\Contracts\View\View;
 use Illuminate\Http\RedirectResponse;
-use Illuminate\Support\Facades\Validator;
 
 class TruckController extends Controller
 {
-    public function __construct()
-    {
-        $this->middleware('auth');
-    }
-
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function index(Request $request)
+    public function index(Request $request): View
     {
         $trucks = Truck::all();
         $truckMakers = Truck::all()->unique('maker');
         $mechanics = Mechanic::all();
-
 
         if ($request->mechanic && $request->mechanic != "default" && $request->truck_maker && $request->truck_maker != "default") {
             $trucks = Truck::where('mechanic_id', $request->mechanic)
@@ -48,52 +39,25 @@ class TruckController extends Controller
         ]);
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
+    public function create(): View
     {
         $mechanics = Mechanic::all();
-        return view('truck.create', ['mechanics' => $mechanics]);
+
+        return view('truck.create', [
+            'mechanics' => $mechanics,
+        ]);
     }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
+    public function store(TruckUpsertRequest $request): RedirectResponse
     {
+        Truck::create([
+            'maker' => $request->truck_maker,
+            'plate' => $request->truck_plate,
+            'make_year' => $request->truck_make_year,
+            'mechanic_notices' => $request->mechanic_notices,
+            'mechanic_id' => $request->mechanic_id,
+        ]);
 
-        $validator = Validator::make(
-            $request->all(),
-            [
-                'truck_maker' => ['required', 'max:255'],
-                'truck_plate' => ['required', 'alpha_dash', 'max:20'],
-                'truck_make_year' => ['required', 'numeric', 'digits:4']
-            ],
-
-            ['truck_maker.required' => 'Truck maker is missing']
-
-        );
-
-        $request->flash();
-
-        if ($validator->fails()) {
-            return redirect()->back()
-                ->withErrors($validator);
-        }
-
-        $truck = new Truck;
-        $truck->maker = $request->truck_maker;
-        $truck->plate = $request->truck_plate;
-        $truck->make_year = $request->truck_make_year;
-        $truck->mechanic_notices = $request->mechanic_notices;
-        $truck->mechanic_id = $request->mechanic_id;
-        $truck->save();
 
         return redirect()
             ->route('truck_index')
@@ -111,15 +75,10 @@ class TruckController extends Controller
         //
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\Models\Truck  $truck
-     * @return \Illuminate\Http\Response
-     */
-    public function edit(Truck $truck)
+    public function edit(Truck $truck): View
     {
         $mechanics = Mechanic::all();
+
         return view('truck.edit', [
             'truck' => $truck,
             'mechanics' => $mechanics,
@@ -127,10 +86,8 @@ class TruckController extends Controller
         ]);
     }
 
-    public function update(TruckUpdateRequest $request, Truck $truck): RedirectResponse
+    public function update(TruckUpsertRequest $request, Truck $truck): RedirectResponse
     {
-        $request->flash();
-
         $truck->update([
             'maker' => $request->truck_maker,
             'plate' => $request->truck_plate,
@@ -139,23 +96,22 @@ class TruckController extends Controller
             'mechanic_id' => $request->mechanic_id
         ]);
 
-        $truck->addMedia($request->file('image'))
-            ->toMediaCollection('image');
+        if ($request->file('image')) {
+            $truck->addMedia($request->file('image'))
+                ->toMediaCollection('image');
+        }
 
-        return redirect()->route('truck_index')
+        return redirect()
+            ->route('truck_index')
             ->with('success_message', 'Sėkmingai pakeista');
     }
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  \App\Models\Truck  $truck
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy(Truck $truck)
+    public function destroy(Truck $truck): RedirectResponse
     {
         $truck->delete();
-        return redirect()->route('truck_index')
+
+        return redirect()
+            ->route('truck_index')
             ->with('success_message', 'Sėkmingai ištrinta');
     }
 }
